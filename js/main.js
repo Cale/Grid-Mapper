2929d7ec01ac157c0566f7dfd2dbd8018902b3f9
Cale <cale@midnightcheese.com>
2019-11-18 21:51:48 -0600

    Adjust styling, fit map to browser window.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 // Initialize Leaflet.js map (https://leafletjs.com/)
-var map = L.map('map').setView([35.85, -88.39], 4);
+var map = L.map('map').setView([35.85, -88.39], 5);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -11,8 +11,8 @@ L.maidenhead({
   precision: 4,
 
   polygonStyle: {
-		color: "#444444",
-		weight: 0.25,
+		color: "#ffffff",
+		weight: .4,
 		fill: true,
 		fillColor: "transparent",
 		fillOpacity: 0,
@@ -32,7 +32,9 @@ $(function () {
 
     L.rectangle([[coords[0], coords[1]], [coords[2], coords[3]]], {
       name: msg,
-      color: "red",
+      color: "#f9f502",
+      fillOpacity: 0.75,
+      stroke: false,
     }).addTo(cqgrids);
   });
 });
@@ -40,7 +42,13 @@ $(function () {
 // Remove grids calling CQ every 15 seconds before new grids are decoded.
 window.setInterval(function() {
     var date = new Date();
-    if(date.getSeconds() === 57 || date.getSeconds() === 12 || date.getSeconds() === 27 || date.getSeconds() === 42) {
+    if(date.getSeconds() === 58 || date.getSeconds() === 13 || date.getSeconds() === 28 || date.getSeconds() === 43) {
         cqgrids.clearLayers();
     }
 }, 1000); // Repeat every second
+
+window.onload = window.onresize = function () {
+    var mapcontainer = document.getElementById("map");
+    var height = window.innerHeight;
+    mapcontainer.style.height = height + "px";
+}
