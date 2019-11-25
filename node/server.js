f250042119e7f3a8bc720a7eeb52e6bb2c1b3c2e
Cale <cale@midnightcheese.com>
2019-11-24 18:05:54 -0600

    Logic to determine whether a QSO is active.

--- node/server.js
+++ node/server.js
@@ -7,8 +7,10 @@ var fs = require('fs');
 Tail = require('tail').Tail;
 var logfile = "/home/cale/.local/share/WSJT-X/ALL.TXT"
 var mycallsign = "K4HCK";
+var mygridsquare = "EM65";
 var workingcallsign = "";
 var workinggridsquare = "";
+var callingcq = false;
 
 app.use('/css', express.static(path.join(__dirname, '../css')));
 app.use('/js', express.static(path.join(__dirname, '../js')));
@@ -25,9 +27,34 @@ io.on('connection', function(socket){
     // Parse single line from WSJT-X ALL.TXT into an array.
     var qso = data.split(" ");
     qso = qso.filter(item => item !== "");
-    console.log(qso);
+    //console.log(qso);
 
-    if (qso.includes("CQ")) {
+    if (qso.includes(mycallsign) && qso[7].includes("CQ")) {
+      // I'm calling CQ / first call.
+      console.log("I'm Calling CQ.");
+      callingcq = true;
+    } else if (qso.includes(mycallsign) && qso.includes(mygridsquare)) {
+      // I'm responding to a CQ call. Grab station's callsign.
+      workingcallsign = qso[7];
+      callingcq = false;
+      console.log("I'm working callsign "+ workingcallsign);
+    } else if (qso.includes(mycallsign) && callingcq) {
+      // My second transmit aftter calling CQ. Grab station's callsign.
+      workingcallsign = qso[8];
+      console.log("My second transmit after calling CQ. Working "+ workingcallsign);
+      callingcq = false;
+      // Get Ham's info.
+    } else if (qso.includes(mycallsign) && qso.includes(workingcallsign) && (qso[9].includes("R-") || qso[9].includes("R+") || qso[9].includes("RRR"))) {
+      // Received report or report recieved.
+      console.log("R or RRR");
+      callingcq = false;
+      // Get Ham's info.
+    } else if (qso.includes(mycallsign) && qso.includes(workingcallsign) && qso[9].includes("73")) {
+      // QSO is ending.
+      callingcq = false;
+      workingcallsign = "";
+      console.log("QSO is ending.");
+    } else if (qso.includes("CQ")) {
         var gridsquare;
         gridsquare = qso[qso.length-1];
 
